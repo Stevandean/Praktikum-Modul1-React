5018833de032a99c4d148017e8d976415c32fad6
Stevandan <stevandeanachmad1@gmail.com>
2022-04-29 12:05:23 +0700

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div className="dashboard ms-5">
+                    <p className='location-header'>Practicum 1/Home</p>
+                    <br></br><br></br><br></br><br></br><br/><br/><br/>
+                         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+               
+            </div>
+                
+    );
+};
+
+export default Home;
